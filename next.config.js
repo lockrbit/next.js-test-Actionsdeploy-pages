f14bd55f96df65f5ab7a5a9e1fa04966bd681def
lockrbit <r03.web.2110099@da.iwasaki.ac.jp>
2023-02-17 16:14:02 +0900

    commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_OPENAI_API_KEY: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
-  },
+    reactStrictMode: true,
+    basePath: process.env.GITHUB_ACTIONS && "/next.js-test-Actionsdeploy-pages",
+    trailingSlash: true,
+    env: {
+        NEXT_PUBLIC_OPENAI_API_KEY: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+    },
 };
 
 module.exports = nextConfig;
